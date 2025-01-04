96911e6e11fac603b9af9d3804ed89e5c8398a8c
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-05 01:07:47 +0200

    Add validation to exercise 5 of Day 1

--- Day 1/user.js
+++ Day 1/user.js
@@ -1,12 +1,37 @@
 function foo() {
-    let username = prompt("username: ");
-    let grades = prompt("grades separated by comma: ");
-    let gradesArr = grades.split(",");
-    console.table(gradesArr);
-    let numsArr = [];
-    for (let i = 0; i < gradesArr.length; i++) {
-        numsArr.push(Number(gradesArr[i]));
+    let rightUser = false;
+    while (!rightUser) {
+        let username = prompt("username: ");
+        if (username && username.trim().length !== 0 && isNaN(Number(username))) {
+            rightUser = true;
+        }
+        else {
+            console.log("please enter a valid username.");
+        }
     }
+
+    let rightGrades = false;
+    let gradesArr = [];
+    while (!rightGrades) {
+        let grades = prompt("grades separated by comma: ");
+        if (!grades) {
+            console.log("invalid, try again.");
+            continue;
+        }
+        else {
+            //split, trim, make sure length of each string's length is not 0
+            gradesArr = grades.split(",").map(grade => grade.trim()).filter(grade => grade.length > 0);
+            //ensure that each element is a number
+            rightGrades = gradesArr.every(grade => !isNaN(Number(grade)));
+            if (!rightGrades) {
+                console.log("invalid, try again");
+            }
+        }
+    }
+
+
+    console.table(gradesArr);
+    let numsArr = gradesArr.map(Number);
     let sum = 0;
     for (let i = 0; i < numsArr.length; i++) {
         sum += numsArr[i];
